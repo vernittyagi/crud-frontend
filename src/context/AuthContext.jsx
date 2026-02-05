a9285a41c043373d600a3ba2a1476944935710b5
vernittyagi <vernittyagi@Vernits-MacBook-Air.local>
2026-02-05 16:13:08 +0530

    fixed pre set token

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState, useContext, createContext } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
-    const [token, setToken] = useState("token");
+    const [token, setToken] = useState(() => localStorage.getItem("token"));
     const [isAuthenticated, setIsAuthenticated] = useState(!!token);
 
     const login = (token) => {
